ecb907f2f937a87fea8f2952829c1ee4cbcef3a7
Maksatbek Bolushov <maksatbek.bolushov@gmail.com>
2022-04-04 21:49:53 +0600

    Network scanner code added

--- 04_network_scanner/network_scanner.py
+++ 04_network_scanner/network_scanner.py
@@ -2,7 +2,28 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    # print(arp_request.summary())
+    # Shows description of this method
+    # scapy.ls(scapy.ARP())
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')  # Creates ethernet packet
+    # scapy.ls(scapy.Ether())
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+
+    return clients_list
 
 
-scan("10.0.2.1/24")
+def print_result(result_list):
+    print("IP\t\t\tMAC address\n------------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
